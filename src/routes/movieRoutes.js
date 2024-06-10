2efe2a919e27d8d64a595c851609dfe268ee7020
Abdullah Alrayes <abdullahalrayes@Abdullahs-MacBook-Air.local>
2024-06-10 16:39:26 +0300

    modify routes file

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
-const { body } = require("express-validator");
 
 router.get("/v1/movies", movieController.listMovies);
+
 router.get(
-  "/v1/movies/:movieId/timeslots/:timeSlotId",
+  "/v1/movies/:movieId/timeslots/:timeSlotId/availability",
   movieController.checkAvailability
 );
-router.post(
+
+router.put(
   "/v1/movies/:movieId/timeslots/:timeSlotId/reserve",
-  body('numberOfPeople').isNumeric(),
   movieController.reserveTimeSlot
 );
 
